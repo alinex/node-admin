45aae68ef737b660a6c9da58d11863885f8db75b
alinex <info@alinex.de>
2018-01-20 18:14:45 +0100

    Colorize logtail and add auto reload

--- src/services/logtail/logtail.service.js
+++ src/services/logtail/logtail.service.js
@@ -1,18 +1,35 @@
 const spawn = require('child_process').spawn;
+const ansi2html = require('ansi-to-html');
+
+const convert = new ansi2html({
+  fg: '#000',
+  bg: '#fff',
+  newline: false,
+  escapeXML: false,
+  stream: false
+});
 
 module.exports = function (app) {
 
   app.get('/logtail', function(req, res) {
   	res.header('Content-Type','text/html;charset=utf-8');
-    res.write('<html><head><title>Logtail - Admin Panel</title></head><body><h1>Logtail</h1><pre>\n')
-
-    tail = spawn('sh', ['-c', 'tail -f ./logs/*']);
+    res.write(`<html>
+      <head>
+        <title>Logtail - Admin Panel</title>
+        <link rel="stylesheet" type="text/css" href="/logtail.css" />
+        <meta http-equiv="refresh content="5">
+      </head>
+      <body>
+        <h1>Logtail</h1>
+        <pre>\n`);
+    tail = spawn('sh', ['-c', 'tail -n 15 -f ./logs/*']);
     tail.stdout.on('data', function(data) {
-  		res.write(data, 'utf-8');
+  		res.write(convert.toHtml(data.toString("utf8"))
+        .replace(/(==> .*? <==)/g, '<h3>$1</h3>'));
       res.flush();
     });
   	tail.stderr.on('data', function(data) {
-  		console.error('stderr: ' + data);
+  		console.error('stderr: ' + convert.toHtml(data));
   	});
   	tail.on('exit', function(code) {
   		res.end();
