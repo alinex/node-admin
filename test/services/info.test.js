4f46c4d678b14e760372842f7e825db0c7c2040f
alinex <info@alinex.de>
2018-01-28 21:38:51 +0100

    Fix eslint style

--- test/services/info.test.js
+++ test/services/info.test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const app = require('../../src/app');
+const assert = require('assert')
+const app = require('../../src/app')
 
 describe('\'info\' service', () => {
   it('registered the service', () => {
-    const service = app.service('info');
+    const service = app.service('info')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
