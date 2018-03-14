489a7b6f193586b0281eab7a1581b2e36d9a3c52
alinex <info@alinex.de>
2018-03-14 21:51:32 +0100

    Small html fixes

--- src/app.js
+++ src/app.js
@@ -102,7 +102,7 @@ app.configure(profiler({
     return {
       request: `${header} ${trailer}`,
       headers: hook.params.headers && Object.keys(hook.params.headers).length ? util.inspect(hook.params.headers) : false,
-      query: Object.keys(hook.params.query).length ? util.inspect(hook.params.query) : false,
+      query: hook.params.query && Object.keys(hook.params.query).length ? util.inspect(hook.params.query) : false,
       data: hook.data ? util.inspect(hook.data) : false,
       error: (hook.error ? clc.red(`${(hook.original || {}).type} ${hook.error ? hook.error.message : ''}`) : false),
       response: hook.result ? util.inspect(hook.result) : false
